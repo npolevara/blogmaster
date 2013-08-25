72fcf17aaf3eade251b10c2212f03fde7996b1a9
Nikita <nipoli8@gmail.com>
2013-08-25 14:02:38 +0300

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogmaster::Application.config.secret_key_base = '15689be4d8e46d20e081a92f3f2f49b45ad6b4088e99c6fb67c748b6021d330f647c8c493dd9aaa5ec9b3cf2dcbb949f8945cf995be33e73cbdf67c68daae2ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blogmaster::Application.config.session_store :cookie_store, key: '_blogmaster_session'
